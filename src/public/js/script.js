b112383c955c0a6d8f63bd707ac3bcf7e49f59e6
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-02-13 16:55:23 +0200

    Reset burger menu and navigation on media change

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,7 +1,18 @@
 function initEventListeners() {
     const burgerMenuElement = document.querySelector('.burger-menu');
+    const mobileMediaQuery = window.matchMedia('(max-width: 768px)');
 
+    mobileMediaQuery.addEventListener('change', onMediaChange);
     burgerMenuElement.addEventListener('click', onBurgerMenuClick);
+}
+
+// Reset burger menu and navigation on media change
+function onMediaChange() {
+    const burgerMenuElement = document.querySelector('.burger-menu');
+    const navContainerElement = document.querySelector('.nav-container');
+
+    navContainerElement.style.display = '';
+    burgerMenuElement.classList.replace('close-menu', 'open-menu');
 }
 
 function onBurgerMenuClick() {
@@ -14,7 +25,7 @@ function onBurgerMenuClick() {
         navContainerElement.style.display = 'block';
     } else {
         burgerMenuElement.classList.replace('close-menu', 'open-menu');
-        navContainerElement.style.display = 'none';
+        navContainerElement.style.display = '';
     }
 }
 
