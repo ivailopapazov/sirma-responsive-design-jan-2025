3150fa667cc536e9b50aaf40b74c582dcbe5d380
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-02-13 16:17:10 +0200

    Show close icon on burger menu click

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -6,8 +6,13 @@ function initEventListeners() {
 
 function onBurgerMenuClick() {
     const navContainerElement = document.querySelector('.nav-container');
+    const burgerMenuElement = document.querySelector('.burger-menu');
 
+    // Show mobile navigation
     navContainerElement.style.display = 'block';
+
+    // Change burger menu icon
+    burgerMenuElement.style.backgroundImage = 'url("/public/img/close.svg")'
 }
 
 initEventListeners();
